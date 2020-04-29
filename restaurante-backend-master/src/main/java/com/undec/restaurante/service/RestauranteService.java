12ab6d1235b04833c68c33becb2378a1c993a93c
Cristian-Caliva <63618028+Cristian-Caliva@users.noreply.github.com>
2020-04-29 15:12:54 -0300

    ListaRestaurantes
    proyecto maven

---
+++ restaurante-backend-master/src/main/java/com/undec/restaurante/service/RestauranteService.java
@@ -0,0 +1,33 @@
+package com.undec.restaurante.service;
+import com.undec.restaurante.model.Restaurante;
+import com.undec.restaurante.repository.RestauranteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+@Service
+public class RestauranteService {
+    @Autowired
+    private RestauranteRepository restauranteRepository;
+    public List<Restaurante> findAll() {
+        List<Restaurante> restauranteList = restauranteRepository.findAll();
+        return restauranteList;
+    }
+
+    public Restaurante findOne(Integer id) {
+        Restaurante restaurante= restauranteRepository.getOne(id);
+        return restaurante;
+    }
+    public String delete(Restaurante r) {
+        restauranteRepository.delete(r);
+        return "Borrado";
+    }
+
+    public String save(Restaurante r) {
+        restauranteRepository.save(r);
+        return "Insertado";
+    }
+
+    public List<Restaurante> BuscarRestaurantePrecio(String precio){
+        return restauranteRepository.findAllByPrecio(precio);
+    }
+}
